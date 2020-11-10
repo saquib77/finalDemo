336f1cf50deb21b921107d2839b1b1bac9be240f
saquib77 <chand567khan@gmail.com>
2020-11-10 17:29:03 +0530

    Initial Message

---
+++ macro.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git add .
+echo "Enter Commit Message"
+read comMes
+git commit -m "${comMes}"
+if [ -n "$(git status - porcelain)"];
+then
+	echo "No Such File to Push"
+else
+	git branch
+	echo "Enter the branch name"
+	read bName
+	git push origin "${bName}"
+	git status
+fi
+echo "Successfull Pushed"
